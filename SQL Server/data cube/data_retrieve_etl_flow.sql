b23ca521f97a70ab86097c274304a223fdc341e0
Samuel Lo <63056991+Samuel123456-stack@users.noreply.github.com>
2025-06-04 17:54:51 -0300

    Create data_retrieve_etl_flow.sql
    Descrição do projeto

---
+++ SQL Server/data cube/data_retrieve_etl_flow.sql
@@ -0,0 +1,22 @@
+--CUBO DE DADOS
+--Definição: 
+  --O cubo de dados é uma estrutura multidimensional amplamente utilizada em sistemas de análise de dados, como Data Warehousing 
+  --e Business Intelligence. Ele organiza informações em dimensões (como tempo, local, produto) e medidas (como vendas, lucros, quantidades), 
+  --permitindo análises e consultas rápidas e complexas.
+    
+  --A principal característica do cubo de dados é sua capacidade de consolidar dados agregados de forma eficiente, 
+  --proporcionando uma visão holística das informações. Essa estrutura possibilita a exploração dos dados sob diferentes perspectivas, 
+  --facilitando a identificação de padrões, tendências e insights valiosos para a tomada de decisões estratégicas.
+
+--Objetivo: Esquematizar uma estrutura de alta flexibilidade, provisionando um grau de liberdade ao cliente em alocar atributos
+--categóricos e quantitativos em diferentes quadrantes, simulando uma planilha dinâmica em virtude das transposições em formato
+--de (linha, coluna)
+
+--TECNOLOGIAS UTILIZADAS
+
+--Ferramentas de análise: Microsoft SQL Server 2019
+--ETL: SSIS (SQL Server Integration Services)
+
+--------------------------------------------------------------------------------------------------------------------------------------
+
+
